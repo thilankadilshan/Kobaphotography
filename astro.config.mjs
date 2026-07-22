bee045c293b3da1aae3664a4c8ef224d9651cc6e
thilankadilshan <thilanka.cv@gmail.com>
2026-07-22 23:00:35 +0530

    vercel deplye Changes to be committed: 	new file:   .vercelignore 	modified:   astro.config.mjs 	new file:   vercel.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import tailwindcss from "@tailwindcss/vite";
+import vercel from "@astrojs/vercel/static";
 
 export default defineConfig({
   integrations: [svelte()],
   output: "static",
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
   },
